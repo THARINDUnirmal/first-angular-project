a62d4cfe81820fd05525d9423c063979db8d1b4e
THARINDUnirmal <tharinduna0@gmail.com>
2025-12-31 16:03:29 +0530

    Project initial

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    standalone: true,
+    selector: 'app-nav',
+    template: "<h1>hi i am Nav Compenet</h1>"
+})
+
+export class NavComponent { }
